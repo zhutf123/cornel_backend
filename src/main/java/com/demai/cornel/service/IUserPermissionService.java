ecda87bc68971e75a8d9c436525c85da7107f240
tengfei.zhu <tengfei.zhu@qunar.com>
2019-10-06 17:44:54 +0800

    声明接口

--- src/main/java/com/demai/cornel/service/IUserPermissionService.java
+++ src/main/java/com/demai/cornel/service/IUserPermissionService.java
@@ -3,8 +3,27 @@
  */
 package com.demai.cornel.service;
 
+import com.demai.cornel.model.RoleInfo;
+import org.apache.ibatis.annotations.Param;
+
 /**
  * Create By zhutf  19-10-6  下午5:37
  */
 public interface IUserPermissionService {
+
+    /**
+     * 根据电话获取用户角色
+     * @param mobile
+     * @return
+     */
+    public RoleInfo getRoleInfoByMobile(@Param("moble")String mobile);
+
+
+    /**
+     * 根据电话获取用户角色
+     * @param opendId
+     * @return
+     */
+    public RoleInfo getRoleInfoByopendId(@Param("openId")String opendId);
+
 }
