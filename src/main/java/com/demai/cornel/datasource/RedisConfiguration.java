b10d1db1109537b9cda646987674d44d335a51b1
tengfei.zhu <tengfei.zhu@qunar.com>
2019-11-06 14:26:27 +0800

    处理根据配置抓取酒店

--- src/main/java/com/demai/cornel/datasource/RedisConfiguration.java
+++ src/main/java/com/demai/cornel/datasource/RedisConfiguration.java
@@ -4,6 +4,8 @@ package com.demai.cornel.datasource;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.demai.cornel.util.json.JsonUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
@@ -22,6 +24,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import com.google.common.base.Splitter;
 
 @Configuration
+@Slf4j
 public class RedisConfiguration {
 
     public static final Splitter COMMA_SPLITTER = Splitter.on(',').trimResults().omitEmptyStrings();
@@ -50,6 +53,10 @@ public class RedisConfiguration {
         String nodes = String.valueOf(propertySource.getProperty("spring.redis.cluster.nodes"));
         List<String> nodeList = COMMA_SPLITTER.splitToList(nodes).stream().map(item -> "redis://" + item)
                 .collect(Collectors.toList());
+        if (log.isDebugEnabled()) {
+            log.debug("获取nodeList:{}:{}", JsonUtil.toJson(nodeList),
+                    String.valueOf(propertySource.getProperty("spring.redis.cluster.password")));
+        }
         Config config = new Config();
         config.useClusterServers()
                 .setPassword(String.valueOf(propertySource.getProperty("spring.redis.cluster.password")))
