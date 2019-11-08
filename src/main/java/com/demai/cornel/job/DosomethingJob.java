2546503e605ee5312c30dc0624f44fcaf52774cf
tengfei.zhu <tengfei.zhu@qunar.com>
2019-11-08 23:38:38 +0800

    add xxl-job

--- src/main/java/com/demai/cornel/job/DosomethingJob.java
+++ src/main/java/com/demai/cornel/job/DosomethingJob.java
@@ -3,6 +3,7 @@
  */
 package com.demai.cornel.job;
 
+import com.xxl.job.core.handler.annotation.JobHandler;
 import lombok.extern.slf4j.Slf4j;
 
 import org.slf4j.Logger;
@@ -17,6 +18,7 @@ import com.xxl.job.core.handler.IJobHandler;
  */
 @Component
 @Slf4j
+@JobHandler("dosomethingJob")
 public class DosomethingJob extends IJobHandler {
     private static final Logger logger = LoggerFactory.getLogger(DosomethingJob.class);
 
