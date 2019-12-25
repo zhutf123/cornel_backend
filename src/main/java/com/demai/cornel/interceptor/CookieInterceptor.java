61c7b7422197ca83d0b603cbb0665464809c5fff
zhutf123 <ztfzjj@163.com>
2019-12-25 18:29:46 +0800

    修改属性类型

--- src/main/java/com/demai/cornel/interceptor/CookieInterceptor.java
+++ src/main/java/com/demai/cornel/interceptor/CookieInterceptor.java
@@ -24,8 +24,7 @@ import static com.demai.cornel.util.CookieAuthUtils.KEY_USER_NAME;
  * Created by binz.zhang on 2019/1/4.
  * 获取用户信息
  */
-@Slf4j @CustomInterceptor(order = 2, addPathPatterns = { "/**" }, excludePathPatterns = { "/check.jsp",
-        "/user/login.json", "/user/sendCode.json" })
+@Slf4j @CustomInterceptor(order = 2, addPathPatterns = { "/**" }, excludePathPatterns = { "/check.jsp" })
 public class CookieInterceptor implements HandlerInterceptor {
 
 
