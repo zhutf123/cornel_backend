fab28561d528fb66ada670863aab9cf55bf57287
binz.zhang <ligongzhangbin@163.com>
2020-01-19 13:59:31 +0800

    update

--- src/main/java/com/demai/cornel/service/DriverRegisterService.java
+++ src/main/java/com/demai/cornel/service/DriverRegisterService.java
@@ -56,9 +56,16 @@ import java.util.UUID;
             imgInfo.setImgId(UUID.randomUUID().toString());
             log.debug("img key is [{}] url is [{}]",x.getKey(),x.getUrl());
             imgInfo.setImgDesc(ImgInfo.IMGDESC.keyOf(x.getKey()).getExpr());
+            log.debug("img key 2",x.getKey(),x.getUrl());
             imgInfo.setUrl(x.getUrl());
+            log.debug("img key 3",x.getKey(),x.getUrl());
+
             imgInfo.setBindId(CookieAuthUtils.getCurrentUser());
+            log.debug("img key 4",x.getKey(),x.getUrl());
+
             imgInfo.setBindType(ImgInfo.BINDTYPESTATUS.BIND_USER.getValue());
+            log.debug("img key 5 ",x.getKey(),x.getUrl());
+
             imgInfoDao.insert(imgInfo);
         });
         driverCpllUserInfoResp.setOptResult(DriverCpllUserInfoResp.STATUS.SUCCESS.getValue());
