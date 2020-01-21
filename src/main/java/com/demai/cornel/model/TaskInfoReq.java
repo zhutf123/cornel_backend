be0bfc081e8063becb2677c77263ff2e2c4edabf
binz.zhang <ligongzhangbin@163.com>
2020-01-21 13:29:45 +0800

    update

--- src/main/java/com/demai/cornel/model/TaskInfoReq.java
+++ src/main/java/com/demai/cornel/model/TaskInfoReq.java
@@ -69,7 +69,7 @@ import java.util.List;
         private int lorryId;
         private String plateNumber;
         private BigDecimal carryWeight;
-        private BigDecimal overCarryWeight;
+        private BigDecimal overCarryWight;
         private String unitWeight;
         private boolean defaultSelect;
         private Integer carStatus;
