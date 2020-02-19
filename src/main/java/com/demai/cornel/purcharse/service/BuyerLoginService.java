a50107682a15586f6fab6dea31735e65ecf2fa20
binz.zhang <ligongzhangbin@163.com>
2020-02-20 00:37:21 +0800

    update

--- src/main/java/com/demai/cornel/purcharse/service/BuyerLoginService.java
+++ src/main/java/com/demai/cornel/purcharse/service/BuyerLoginService.java
@@ -119,7 +119,7 @@ import java.util.concurrent.TimeUnit;
         BeanUtils.copyProperties(buyerCplUserInfoReq, userInfo);
         userInfo.setStatus(UserInfo.USER_STATUS.ENABLE.getValue());
         userInfo.setUserId(CookieAuthUtils.getCurrentUser());
-        userInfo.setMobile(Sets.newHashSet(userInfo.getMobile()));
+        userInfo.setMobile(Sets.newHashSet(buyerCplUserInfoReq.getMobile()));
         int res = buyerInfoMapper.updateByPrimaryKey(userInfo);
         if (res == 0) {
             log.debug("driver register complete fail due to update user info fail");
