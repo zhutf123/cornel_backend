c39f5034136598381cb8c31544ad1a1556144d9e
binz.zhang <ligongzhangbin@163.com>
2020-02-20 19:23:05 +0800

    update

--- src/main/java/com/demai/cornel/purcharse/vo/resp/BuyerCommitListResp.java
+++ src/main/java/com/demai/cornel/purcharse/vo/resp/BuyerCommitListResp.java
@@ -16,5 +16,6 @@ public class BuyerCommitListResp extends Commodity {
     private String receiveStartTime;//预估收货时间
     private String receiveEndTime;//预估收货结束时间
     private String unitWeight="吨"; //价格单位
+    private BigDecimal shipmentWeight=30.00;
 
 }
