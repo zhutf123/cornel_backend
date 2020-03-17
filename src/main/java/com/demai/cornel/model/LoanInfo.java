e7310d7cf20607af7cf2b4ed0fa4a560fe9b3b9e
binz.zhang <ligongzhangbin@163.com>
2020-03-17 16:09:30 +0800

    update

---
+++ src/main/java/com/demai/cornel/model/LoanInfo.java
@@ -0,0 +1,85 @@
+package com.demai.cornel.model;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.Date;
+import lombok.Data;
+
+/**
+* @Author binz.zhang
+* @Date: 2020-03-17    09:49
+*/
+@Data
+public class LoanInfo {
+    /**
+    * 自增ID
+    */
+    private Integer id;
+
+    private String loanId;
+
+    /**
+    * 贷款金额
+    */
+    private BigDecimal price;
+
+    /**
+    * 利息
+    */
+    private BigDecimal interest;
+
+    /**
+    * 贷款人
+    */
+    private String applyUserId;
+
+    /**
+    * 请款日期
+    */
+    private Timestamp applyTime;
+
+    /**
+    * 放款日期
+    */
+    private Timestamp lendingTime;
+
+    /**
+    * 预计还款日期
+    */
+    private Timestamp estRepayTime;
+
+    /**
+    * 实际还款日期
+    */
+    private Timestamp actualRepayTime;
+
+    /**
+    * 利息总和
+    */
+    private BigDecimal interestTotal;
+
+    /**
+    * 还款金额
+    */
+    private BigDecimal repayPrice;
+
+    /**
+    * 审批人
+    */
+    private String reviewUserId;
+
+    /**
+    * 创建时间
+    */
+    private Timestamp createTime;
+
+    /**
+    * 更新时间
+    */
+    private Timestamp updateTime;
+
+    /**
+    * 状态0 已取消 1待审核 2审核通过 3审核拒绝
+    */
+    private Integer status;
+}
