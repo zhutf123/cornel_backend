487f610fbc042d813fd15669317b044b6efe632c
binz.zhang <ligongzhangbin@163.com>
2020-03-18 22:14:36 +0800

    update

--- src/main/java/com/demai/cornel/vo/quota/GetDryWetRadioResp.java
+++ src/main/java/com/demai/cornel/vo/quota/GetDryWetRadioResp.java
@@ -1,9 +1,12 @@
 package com.demai.cornel.vo.quota;
 
+import lombok.Data;
+
 /**
  * @Author binz.zhang
  * @Date: 2020-03-18    21:16
  */
+@Data
 public class GetDryWetRadioResp {
     private float dryWetRadio = 1.2f;
 }
