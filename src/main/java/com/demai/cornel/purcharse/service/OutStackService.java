404e68a56ac872e7feec2b22eb025e7dc210e417
binz.zhang <ligongzhangbin@163.com>
2020-04-03 13:33:03 +0800

    update 小工具 增加系统报价 或者个人报价的接口

--- src/main/java/com/demai/cornel/purcharse/service/OutStackService.java
+++ src/main/java/com/demai/cornel/purcharse/service/OutStackService.java
@@ -80,6 +80,58 @@ import java.util.UUID;
         return stackOutInfo;
     }
 
+    public StackOutInfo buildSystemDefaultOutStackInfo(SaleOrder saleOrder) {
+        List<StoreInfo> storeInfos = storeInfoMapper
+                .selectStoreIdByCommodityIdAndWeight(saleOrder.getCommodityId(), saleOrder.getWeight());
+        if (storeInfos == null || storeInfos.size() == 0) {
+            return null;
+        }
+        BigDecimal minTotalPrice = null;
+        StoreInfo optimumStoreId = null;
+        FreightInfo optimumFreight = null;
+        for (StoreInfo storeInfo : storeInfos) {
+            FreightInfo freightInfo = locationService
+                    .getPriceOptimum(storeInfo.getLocationId(), saleOrder.getReceiveLocation());
+            if (freightInfo != null && freightInfo.getPrice() != null) {
+                BigDecimal totalPrice = storeInfo.getBuyingPrice().add(freightInfo.getPrice())
+                        .add(storeInfo.getCapitalCost());
+                if (minTotalPrice == null) {
+                    minTotalPrice = totalPrice;
+                    optimumStoreId = storeInfo;
+                    optimumFreight = freightInfo;
+                } else {
+                    minTotalPrice = minTotalPrice.compareTo(totalPrice) == -1 ? totalPrice : minTotalPrice;
+                    optimumStoreId = storeInfo;
+                    optimumFreight = freightInfo;
+                }
+            }
+        }
+        if (optimumStoreId == null) {
+            log.warn("buildSystemDefaultOutStackInfo fail due to can not find the store");
+            return null;
+        }
+        StackOutInfo stackOutInfo = new StackOutInfo();
+        stackOutInfo.setStoreId(optimumStoreId.getStoreId());
+        stackOutInfo.setOutId(UUID.randomUUID().toString());
+        stackOutInfo.setBuyerId(saleOrder.getBuyerId());
+        stackOutInfo.setCommodityId(saleOrder.getCommodityId());
+        stackOutInfo.setFromLocation(optimumStoreId.getLocationId());
+        stackOutInfo.setReceiveLocation(saleOrder.getReceiveLocation());
+        stackOutInfo.setFreightPrice(optimumFreight.getPrice());
+        stackOutInfo.setFreightInfoId(optimumFreight.getFreightId());
+        stackOutInfo.setOrderPrice(saleOrder.getOrderPrice());
+        stackOutInfo.setUnitPrice(saleOrder.getUnitPrice());
+        stackOutInfo.setUnitWeight(saleOrder.getUnitWeight());
+        int res = stackOutInfoMapper.insertSelective(stackOutInfo);
+        if (res != 1) {
+            log.warn("buildSystemDefaultOutStackInfo fail due to insert stackOutInfo fail ");
+            return null;
+        }
+        saleOrder.setOutStackId(stackOutInfo.getOutId());
+        saleOrder.setEsIncome(saleOrder.getCommodityPrice().subtract(minTotalPrice));
+        return stackOutInfo;
+    }
+
 //    public StackOutInfo buildSystemDefaultOutStackInfo(AdminUnRevSaleDetail saleOrder) {
 //        SaleOrder saleOrder1 = new SaleOrder();
 //        BeanUtils.copyProperties(saleOrder, saleOrder1);
