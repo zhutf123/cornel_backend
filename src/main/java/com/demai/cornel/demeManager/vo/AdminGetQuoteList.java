a286acb67bfae9030c299a674431649fb4373397
binz.zhang <ligongzhangbin@163.com>
2020-04-08 15:58:31 +0800

    update 小工具 增加系统报价 或者个人报价的接口

--- src/main/java/com/demai/cornel/demeManager/vo/AdminGetQuoteList.java
+++ src/main/java/com/demai/cornel/demeManager/vo/AdminGetQuoteList.java
@@ -15,11 +15,22 @@ import java.util.List;
     private List<orderInfo> tower_info;
 
     @Data public  class orderInfo {
-        private long count;
+        private Long count;
         private Integer status;
         private String unit_price="元";
         private String unit_weight="吨";
         private BigDecimal weight;
         private BigDecimal total;
+
+        public orderInfo(Long count, Integer status,BigDecimal weight,
+                BigDecimal total) {
+            this.count = count;
+            this.status = status;
+            this.unit_price = unit_price;
+            this.unit_weight = unit_weight;
+            this.weight = weight;
+            this.total = total;
+        }
     }
+
 }
