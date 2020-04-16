126010af4f5b6a41ed914031067f749c763afbeb
binz.zhang <ligongzhangbin@163.com>
2020-04-16 23:39:22 +0800

    update 小工具 增加系统报价 或者个人报价的接口

--- src/main/java/com/demai/cornel/demeManager/vo/AdminFinishSaleDetail.java
+++ src/main/java/com/demai/cornel/demeManager/vo/AdminFinishSaleDetail.java
@@ -13,8 +13,7 @@ import java.math.BigDecimal;
  * @Author binz.zhang
  * @Date: 2020-04-03    13:13
  */
-@Data @Builder @AllArgsConstructor @NoArgsConstructor
-public class AdminFinishSaleDetail extends AdminFinishSaleList{
+@Data @Builder @AllArgsConstructor @NoArgsConstructor public class AdminFinishSaleDetail extends AdminFinishSaleList {
     private BigDecimal buyingPrice;
     private BigDecimal freightPrice;
     private BigDecimal capitalCost;// 资金使用成本
@@ -22,11 +21,13 @@ public class AdminFinishSaleDetail extends AdminFinishSaleList{
     private Integer shippProcess;
     private String shippProcessKey;
 
+
     public void setShippProcess(Integer shippProcess) {
         this.shippProcess = shippProcess;
         ShippProcess.TYPE type = ShippProcess.TYPE.keyOf(shippProcess);
         this.shippProcessKey = type == null ? "" : type.getExpr();
     }
+
     public static enum STATUS_ENUE implements IEmus {
         PARAM_ERROR(-1, "参数错误"), SUCCESS(0, "请求成功"), ORDER_INVALID(1, "订单无效"), USER_ERROR(2, "用户无权限"), SERVER_ERROR(3,
                 "服务器异常");
