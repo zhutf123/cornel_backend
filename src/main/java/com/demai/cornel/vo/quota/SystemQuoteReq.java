e8ad3d52d1dfc48c8f49303ccdfbd35a9d825bf7
zhutf-pro <tengfei.zhu@qunar.com>
2020-11-24 22:15:20 +0800

     添加业务人员操作姓名和时间

--- src/main/java/com/demai/cornel/vo/quota/SystemQuoteReq.java
+++ src/main/java/com/demai/cornel/vo/quota/SystemQuoteReq.java
@@ -2,10 +2,12 @@ package com.demai.cornel.vo.quota;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @Author binz.zhang
  * @Date: 2020-01-06    20:06
  */
 @Data
-public class SystemQuoteReq extends OfferQuoteReq {
+public class SystemQuoteReq extends OfferQuoteReq implements Serializable {
 }
